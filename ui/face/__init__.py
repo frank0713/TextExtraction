61b3471c1f12948376bb9bb23525bfcc6aaa7d33
qin xu <xuqin198789@outlook.com>
2020-03-09 10:36:13 +0800

    ui design

---
+++ ui/face/__init__.py
@@ -0,0 +1,3 @@
+from . import mainform
+__version__='0.1'
+__all__=['mainform']
---
+++ ui/face/mainform.py
@@ -0,0 +1,12 @@
+import wx
+
+class TextMainForm(wx.Frame):
+    # 寻找框1
+    # 寻找按钮1
+    # 文件选择框1
+    # 文件选择按钮1
+    # 文件排序按钮4个，按时间排序，按匹配度排序，按大小排序，默认排序
+    # 左侧文件展示框1（大）
+    # 右侧文件内容显示框1（小）
+    def __init__(self, parent, id=ID_ANY, title=EmptyString, pos=DefaultPosition, size=DefaultSize, style=DEFAULT_FRAME_STYLE, name=FrameNameStr):
+        pass
